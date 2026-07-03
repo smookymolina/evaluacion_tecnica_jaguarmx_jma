a06f92d25e051060708638a9dd620e927b16f7bd
smookymolina <164466092+smookymolina@users.noreply.github.com>
2026-07-03 13:57:03 -0600

    Limpiar versiones anteriores de PCB y scripts en KiCad

--- kicad/PCB/inspect_api.py
+++
@@ -1,8 +0,0 @@
-import pcbnew
-board = pcbnew.LoadBoard(r"C:\Users\GIRTEC\Claude\Projects\Jaguar MX\kicad\PCB\PCBv6.kicad_pcb")
-
-# Find H1
-fp = board.FindFootprintByReference("H1")
-print(dir(board))
-print("---")
-print(dir(fp))
--- kicad/PCB/test_fp2.py
+++
@@ -1,8 +0,0 @@
-import pcbnew
-
-try:
-    fp = pcbnew.FootprintLoad(r"C:\Program Files\KiCad\10.0\share\kicad\footprints\MountingHole.pretty", "MountingHole_3.2mm_M3_Pad")
-    if fp:
-        print("Success loading MountingHole_3.2mm_M3_Pad")
-except Exception as e:
-    print(f"Error: {e}")
